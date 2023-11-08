f1b0bf8721214ee6f49cc12e0aed41247401dac7
RugvedKute <rugvedkute02@gmail.com>
2023-11-08 12:07:01 +0530

    New token

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,6 +1,6 @@
 export const Constants = {
   API_URL: 'https://api.github.com/users/',
-  TOKEN: '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',
+  TOKEN: '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',
   GLOBAL_LOADER_FGSSIZE: 0,
   GLOBAL_LOADER_BGSOPACITY: 0.1,
   GLOBAL_LOADER_OVERLAYCOLOR: 'rgba(40, 40, 40, 0)',
