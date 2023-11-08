e0b46ccc9350ee3f65c17c1e0412bdd7d7123098
RugvedKute <97442923+RugvedKute@users.noreply.github.com>
2023-11-08 12:17:32 +0530

    Update constants.ts

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,6 +1,6 @@
 export const Constants = {
   API_URL: 'https://api.github.com/users/',
-  TOKEN: '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',
+  TOKEN: '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',
   GLOBAL_LOADER_FGSSIZE: 0,
   GLOBAL_LOADER_BGSOPACITY: 0.1,
   GLOBAL_LOADER_OVERLAYCOLOR: 'rgba(40, 40, 40, 0)',
