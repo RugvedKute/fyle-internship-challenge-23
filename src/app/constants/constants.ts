ef09a151f646801ad4970126c808abf87a1307bd
RugvedKute <rugvedkute02@gmail.com>
2023-11-08 12:23:37 +0530

    token issue

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,6 +1,6 @@
 export const Constants = {
   API_URL: 'https://api.github.com/users/',
-  TOKEN: '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',
+  TOKEN: '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',
   GLOBAL_LOADER_FGSSIZE: 0,
   GLOBAL_LOADER_BGSOPACITY: 0.1,
   GLOBAL_LOADER_OVERLAYCOLOR: 'rgba(40, 40, 40, 0)',
